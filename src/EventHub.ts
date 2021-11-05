f1789886ec7b06532dc88e9bfce0a7a02dd9965e
Christine Walsh <christine.walsh@accenture.com>
2021-11-05 16:01:35 -0400

    Datastillery/hercules#85

--- src/EventHub.ts
+++ src/EventHub.ts
@@ -2,14 +2,14 @@ import { EventHubProducerClient } from '@azure/event-hubs';
 import { getEventHubUrl } from './configuration';
 import { Logger } from './Logger';
 
-const eventHubName = 'micnotppasevn01';
+const eventHubName = 'urban-os';
 const producer = new EventHubProducerClient(getEventHubUrl(), eventHubName);
 
 export class EventHub {
     public sendToEventHub = async (msg: any): Promise<void> => {
         Logger.azureSendAttempt();
         const batch = await producer.createBatch();
-        batch.tryAdd(msg);
+        batch.tryAdd({ body: msg });
         await producer
             .sendBatch(batch)
             .then(Logger.azureSendSuccess)
